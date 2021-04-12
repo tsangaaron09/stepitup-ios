3477c9edaed13f806fe46f3e4fcd296b2c604c96
nikolabo <nikolabo@live.com>
2021-04-12 15:52:21 -0700

    gitignore

---
+++ stepitup/stepitup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stepitup
+//
+//  Created by Nikola Bojanic on 4/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
